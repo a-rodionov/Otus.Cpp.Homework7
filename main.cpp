ab5fce2b4268e9a0c09d0892c3381907d2532c02
Andrew Rodionov <camelot58@gmail.com>
2018-05-13 03:53:13 -0700

    - добавлена проверка параметра командной строки.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <limits>
 #include "Storage.h"
 #include "ConsoleOutput.h"
 #include "FileOutput.h"
@@ -7,12 +9,33 @@ int main(int argc, char const* argv[])
 {
   try
   {
-    if(2 != argc)
-      throw std::invalid_argument("Only one positive integer must be provided as programm parameter.");
+    unsigned long long block_size;
+    try
+    {
+      if(2 != argc)
+        throw std::invalid_argument("");
 
-    std::string block_size{argv[1]};
+      std::string digit_str{argv[1]};
+      if(!std::all_of(std::cbegin(digit_str),
+                      std::cend(digit_str),
+                      [](unsigned char symbol) { return std::isdigit(symbol); } ))
+        throw std::invalid_argument("");
+
+      block_size = std::stoull(digit_str);
+      if(0 == block_size)
+        throw std::invalid_argument("");
+    }
+    catch(...)
+    {
+      std::ostringstream error_msg;
+      error_msg << "The programm must be started with only one parameter. It must be a digit from 1 to "
+                << std::numeric_limits<decltype(block_size)>::max()
+                << " in decimal base.";
+      throw std::invalid_argument(error_msg.str());
+    }
+
     auto commandProcessor = std::make_unique<CommandProcessor>();
-    std::shared_ptr<Storage> storage = std::make_shared<Storage>(std::stoull(block_size));
+    std::shared_ptr<Storage> storage = std::make_shared<Storage>(block_size);
     std::shared_ptr<IOutput> consoleOutput = std::make_shared<ConsoleOutput>(std::cout);
     std::shared_ptr<IOutput> fileOutput = std::make_shared<FileOutput>();
 
