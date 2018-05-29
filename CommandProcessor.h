0d1ef5b8334cf3f33a348d793d12919f7d1d399e
Andrew Rodionov <camelot58@gmail.com>
2018-05-29 03:53:39 -0700

    - Одиночные строки после условий if/else обрамлены фигурными скобками.

--- CommandProcessor.h
+++ CommandProcessor.h
@@ -11,22 +11,26 @@ public:
     for(std::string command; std::getline(in, command);) {
       if(!command.empty()) {
         if("{" == command) {
-          if(0 == open_brace_count++)
+          if(0 == open_brace_count++) {
             BlockStart();
+          }
           continue;
         }
         if("}" == command) {
-          if(0 == open_brace_count)
+          if(0 == open_brace_count){
             BlockEnd();
-          else if(0 == --open_brace_count)
+          }
+          else if(0 == --open_brace_count) {
             BlockEnd();
+          }
           continue;
         }
       }
       Push(command);
     }
-    if(0 == open_brace_count)
+    if(0 == open_brace_count) {
       Flush();
+    }
   }
 
 private:
