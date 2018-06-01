69f9b200bfc154ff246ace57453b4c9bc846be6b
Andrew Rodionov <camelot58@gmail.com>
2018-06-01 14:37:18 -0700

    - реализован шаблонный метод с вызовом лямбд функций для избежания повторения кода.

--- StorageObservable.h
+++ StorageObservable.h
@@ -3,41 +3,41 @@
 #include "IStorage.h"
 #include "Observable.h"
 
+auto flush_notification = [] (const std::shared_ptr<IStorage>& subscriber) { subscriber->Flush(); };
+auto block_start_notification = [] (const std::shared_ptr<IStorage>& subscriber) { subscriber->BlockStart(); };
+auto block_end_notification = [] (const std::shared_ptr<IStorage>& subscriber) { subscriber->BlockEnd(); };
+
 class StorageObservable : public Observable<IStorage>
 {
 
 protected:
 
   void Push(const std::string& data) {
-    for (const auto& subscriber : subscribers) {
-      auto subscriber_locked = subscriber.lock();
-      if(subscriber_locked)
-        subscriber_locked->Push(data);
-    }
+    Notify( [&data] (const std::shared_ptr<IStorage>& subscriber) { subscriber->Push(data); } );
   }
 
   void Flush() {
-    for (const auto& subscriber : subscribers) {
-      auto subscriber_locked = subscriber.lock();
-      if(subscriber_locked)
-        subscriber_locked->Flush();
-    }
+    Notify(flush_notification);
   }
 
   void BlockStart() {
-    for (const auto& subscriber : subscribers) {
-      auto subscriber_locked = subscriber.lock();
-      if(subscriber_locked)
-        subscriber_locked->BlockStart();
-    }
+    Notify(block_start_notification);
   }
 
   void BlockEnd() {
+    Notify(block_end_notification);  
+  }
+
+private:
+
+  template<typename Callable>
+  void Notify(Callable&& callable) {
      for (const auto& subscriber : subscribers) {
       auto subscriber_locked = subscriber.lock();
-      if(subscriber_locked)
-        subscriber_locked->BlockEnd();
-    }   
+      if(subscriber_locked) {
+        callable(subscriber_locked);
+      }
+    }  
   }
 
 };
